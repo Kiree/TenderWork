7fc85adfe598a5b4e719e20546fc154edcd208bb
Jukka Länsineva <jukka.lansineva@student.oulu.fi>
2016-02-09 13:38:18 +0200

    oikeat filut mukaan

--- src/main/webapp/scripts/app/entities/project/project.controller.js
+++ src/main/webapp/scripts/app/entities/project/project.controller.js
@@ -4,8 +4,8 @@ angular.module('tenderworkApp')
     .controller('ProjectController', function ($scope, $state, Project, ProjectSearch, ParseLinks) {
 
         $scope.projects = [];
-        $scope.predicate = 'id';
-        $scope.reverse = true;
+        $scope.predicate = 'editedDate';
+        $scope.reverse = false;
         $scope.page = 0;
         $scope.loadAll = function() {
             Project.query({page: $scope.page, size: 20, sort: [$scope.predicate + ',' + ($scope.reverse ? 'asc' : 'desc'), 'id']}, function(result, headers) {
@@ -23,6 +23,7 @@ angular.module('tenderworkApp')
         $scope.loadPage = function(page) {
             $scope.page = page;
             $scope.loadAll();
+
         };
         $scope.loadAll();
 
--- src/main/webapp/scripts/app/entities/project/project.js
+++ src/main/webapp/scripts/app/entities/project/project.js
@@ -129,3 +129,4 @@ angular.module('tenderworkApp')
                 }]
             });
     });
+
