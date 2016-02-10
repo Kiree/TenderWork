e23733d23931cf4b3182d715087b937002e2a62d
Jukka Länsineva <jukka.lansineva@student.oulu.fi>
2016-02-10 17:59:51 +0200

    created/edited by users/dates now saved automatically

--- src/main/webapp/scripts/app/entities/project/project-dialog.controller.js
+++ src/main/webapp/scripts/app/entities/project/project-dialog.controller.js
@@ -1,15 +1,34 @@
 'use strict';
 
 angular.module('tenderworkApp').controller('ProjectDialogController',
-    ['$scope', '$stateParams', '$uibModalInstance', 'entity', 'Project', 'User',
-        function($scope, $stateParams, $uibModalInstance, entity, Project, User) {
+    ['$scope', '$stateParams', '$uibModalInstance', 'entity', 'Project', 'User', 'Principal',
+        function($scope, $stateParams, $uibModalInstance, entity, Project, User, Principal) {
 
         $scope.project = entity;
         $scope.users = User.query();
+
         $scope.load = function(id) {
             Project.get({id : id}, function(result) {
                 $scope.project = result;
             });
+        };
+
+        Principal.identity().then(function(account) {
+            $scope.myAccount = copyAccount(account);
+            User.get({login: $scope.myAccount.login}, function(result) {
+                $scope.currentUserAccount = result;
+            });
+        });
+
+        var copyAccount = function(account) {
+            return {
+                activated:account.activated,
+                email:account.email,
+                firstName:account.firstName,
+                langKey: account.langKey,
+                lastName: account.lastName,
+                login: account.login
+            }
         };
 
         var onSaveSuccess = function (result) {
@@ -23,10 +42,15 @@ angular.module('tenderworkApp').controller('ProjectDialogController',
         };
 
         $scope.save = function () {
+            var today = new Date();
+            $scope.project.editedBy = $scope.currentUserAccount;
+            $scope.project.editedDate = today;
             $scope.isSaving = true;
             if ($scope.project.id != null) {
                 Project.update($scope.project, onSaveSuccess, onSaveError);
             } else {
+                $scope.project.createdDate = today;
+                $scope.project.createdBy = $scope.currentUserAccount;
                 Project.save($scope.project, onSaveSuccess, onSaveError);
             }
         };
