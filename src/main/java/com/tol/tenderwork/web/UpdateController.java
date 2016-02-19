29cfc3a609e78a9e30e065ba9ccd36fb4e42a577
Petteri Salonurmi <petteri.salonurmi@hotmail.com>
2016-02-19 11:27:16 +0200

    Taskien yksittäisten totalien laskukaavan pyöristys

--- src/main/java/com/tol/tenderwork/web/UpdateController.java
+++ src/main/java/com/tol/tenderwork/web/UpdateController.java
@@ -8,10 +8,9 @@ import com.tol.tenderwork.domain.Task;
 
 /**
  * Created by Sebastian Körkkö on 18.2.2016.
+ * Edited by Petteri Salonurmi on 19.2.2016 11:26 GMT+2.
  */
-/**
-* Slack Integration Test
-*/
+
 public class UpdateController {
 
     public Project updateProject(Project project){
@@ -33,13 +32,14 @@ public class UpdateController {
 
         if(task.getSynergyCheck() == true) {
             float synergyHelper = task.getEstimateSynergy() * task.getSynergyBenefit().getSynergyBenefit();
-            task.setSynergyTotal((int) synergyHelper);
+            task.setSynergyTotal(Math.round(synergyHelper));
         }
 
         float impFactorHelper = (float)task.getEstimateImplementation() * task.getImplementationFactor().getImplementationFactor();
         float testFactorHelper = (float)task.getEstimateTesting() * task.getTestingFactor().getTestingFactor();
         float specFactorHelper = (float)task.getEstimateSpecification() * task.getSpecificationFactor().getSpecificationFactor();
-        task.setEstimateTotal((int) impFactorHelper + (int) testFactorHelper + (int) specFactorHelper);
+        //task.setEstimateTotal((int) impFactorHelper + (int) testFactorHelper + (int) specFactorHelper);
+        task.setEstimateTotal(Math.round((impFactorHelper + testFactorHelper + specFactorHelper)));
         return task;
     }
 
