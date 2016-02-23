cbcc7691125d1b7c2312e03331d80b8011fcbea9
Jukka Länsineva <jukka.lansineva@student.oulu.fi>
2016-02-23 22:04:53 +0200

    fix: most back-buttons fixed

---
+++ src/main/webapp/scripts/components/easter/easter.js
@@ -0,0 +1,17 @@
+/**
+ * Created by jukka on 23.2.2016.
+ */
+
+angular.module('tenderworkApp').controller('EasterController', ['$scope', '$rootScope', function($scope, $rootScope) {
+    var quotes = ['Think different!', 'Work!', 'Null Pointer!', 'Revert!', 'Project Manage!'];
+    var current_index = 0;
+    $rootScope.$on('$stateChangeStart', function (event, toState, toStateParams) {
+      $scope.newIndex();
+    });
+    $scope.newIndex = function() {
+      current_index =   Math.floor(Math.random() * quotes.length);
+    };
+    $scope.randomQuote = function() {
+        return quotes[current_index];
+    };
+}]);
