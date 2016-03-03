5eda40b96ff2fe9ef8b8e54fd9a4097c9d908bad
Jukka Länsineva <jukka.lansineva@student.oulu.fi>
2016-03-03 22:34:24 +0200

    hacks for borders that kinda work

--- src/main/webapp/scripts/app/entities/project/project-detail.controller.js
+++ src/main/webapp/scripts/app/entities/project/project-detail.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('tenderworkApp')
-    .controller('ProjectDetailController', function ($scope, $rootScope, $stateParams, entity, Project, User, Estimate, ParseLinks, EstimateSearch, $translate) {
+    .controller('ProjectDetailController', function ($window, $scope, $rootScope, $stateParams, entity, Project, User, Estimate, ParseLinks, EstimateSearch, $translate) {
         $scope.project = entity;
         $scope.estimates = [];
         $scope.predicate = 'id';
@@ -12,6 +12,21 @@ angular.module('tenderworkApp')
                 $scope.project = result;
             });
         };
+        // 768 is the limit for change
+        // @ 1200 is the max
+        var calculateHeight = function() {
+            if($scope.project.docLocation===undefined) {
+                return 0;
+            }
+            var initial_value = 150;
+            var row_height = 8;
+            var width = $window.innerWidth < 1200 ? $window.innerWidth : 1200;
+            width = width < 768 ? 768 : width;
+            width = width < 992 ? 992 * .2 : width;
+            var multiplier = 1200 / width;
+            var approximate_rows = Math.ceil($scope.project.docLocation.length / (100 * multiplier));
+            return initial_value + (row_height * multiplier) * approximate_rows; //$scope.calculated_height = initial_value * multiplier;
+        };
 
         // takes float and rounds it to nearest whole or half
         var roundResourcing = function(resourcing) {
@@ -19,6 +34,7 @@ angular.module('tenderworkApp')
         };
 
         $scope.loadAll = function() {
+            $scope.calculated_height = calculateHeight();
             EstimateSearch.query({query:"ownerProject.id:" + $scope.project.id}, function(result) {
                 for (var i = 0; i < result.length; i++) {
                     if(result[i].resourcing !== null && entity.resourcing !== undefined) {
@@ -47,7 +63,9 @@ angular.module('tenderworkApp')
         };
         $scope.loadAll();
 
-
+        $rootScope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams) {
+            $scope.calculated_height = calculateHeight();
+        });
 
         var unsubscribe = $rootScope.$on('tenderworkApp:projectUpdate', function(event, result) {
             $scope.project = result;
