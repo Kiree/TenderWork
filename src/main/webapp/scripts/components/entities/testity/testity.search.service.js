340e85a624d66451af5f8654d56c924131da513d
Petteri Salonurmi <petteri.salonurmi@hotmail.com>
2016-03-22 13:12:03 +0200

    Cleared away the the rotting Testity corpse

--- src/main/webapp/scripts/components/entities/testity/testity.search.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('tenderworkApp')
-    .factory('TestitySearch', function ($resource) {
-        return $resource('api/_search/testitys/:query', {}, {
-            'query': { method: 'GET', isArray: true}
-        });
-    });
