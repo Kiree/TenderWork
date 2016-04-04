86b7b295a6632709b6de42e277f4b48f0222ae74
Petteri Salonurmi <petteri.salonurmi@hotmail.com>
2016-04-04 15:30:25 +0300

    Project search now only searches the project name and client fields. Todo: tag searching

--- src/main/java/com/tol/tenderwork/repository/search/ProjectSearchRepository.java
+++ src/main/java/com/tol/tenderwork/repository/search/ProjectSearchRepository.java
@@ -1,10 +1,20 @@
 package com.tol.tenderwork.repository.search;
 
 import com.tol.tenderwork.domain.Project;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+import java.util.List;
 
 /**
  * Spring Data ElasticSearch repository for the Project entity.
  */
 public interface ProjectSearchRepository extends ElasticsearchRepository<Project, Long> {
+
+    //@Query("{\"bool\": {\"should\": [{\"match\": {\"project.name\": \"?0\"}}, {\"match\": {\"project.client\"}}]}}")
+    Iterable<Project> findByNameOrClientOrTags(String name, String client, String tag);
+
 }
