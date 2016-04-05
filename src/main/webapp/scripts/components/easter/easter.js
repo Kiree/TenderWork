5da25e3d2ceefcc36f79b7c75f22db21840a1f0a
Jukka Länsineva <jukka.lansineva@student.oulu.fi>
2016-04-05 12:29:18 +0300

    +disabled entities from navbar +tsygedeliaa

--- src/main/webapp/scripts/components/easter/easter.js
+++ src/main/webapp/scripts/components/easter/easter.js
@@ -2,7 +2,7 @@
  * Created by jukka on 23.2.2016.
  */
 
-angular.module('tenderworkApp').controller('EasterController', ['$scope', '$rootScope', function($scope, $rootScope) {
+angular.module('tenderworkApp').controller('EasterController', ['$scope', '$rootScope', '$interval', function($scope, $rootScope, $interval) {
     var quotes = ['Think different!', 'Work!', 'Null Pointer!', 'Revert!', 'Project Manage!', 'Super Fusion!',
         'but not with Firefox!', 'do juttuja!', 'work in the right order!', '...coffee.', 'cause issues!',
         '...paitsi jos Sebastian - niijoo', 'j', 'koo', '... no nyt mää unohin'];
@@ -19,4 +19,26 @@ angular.module('tenderworkApp').controller('EasterController', ['$scope', '$root
     $scope.randomHours = function() {
         return Math.floor(Math.random() * 50);
     };
+    var LSDwhite = 10;
+    var LSDpurple = 0;
+    var LSDpink = 5;
+    $scope.LSD = function() {
+        if(LSDpurple > 10) {
+            $('.well').css('background', 'repeating-radial-gradient(#ff33ff '+LSDpink+'%, #ffffff '+LSDwhite+'%, #aa55ff '+LSDpurple+'%,#ff33ff '+(LSDpink+15)+'%');
+        } else if(LSDpurple > 5) {
+            $('.well').css('background', 'repeating-radial-gradient(#ffffff '+LSDwhite+'%, #aa55ff '+LSDpurple+'%, #ff33ff '+LSDpink+'%,#ffffff '+(LSDwhite+15)+'%');
+        } else {
+            $('.well').css('background', 'repeating-radial-gradient(#aa55ff '+LSDpurple+'%, #ff33ff '+LSDpink+'%, #ffffff '+LSDwhite+'%,#aa55ff '+(LSDpurple+15)+'%');
+        }
+        LSDwhite += 1;
+        LSDpurple += 1;
+        LSDpink += 1;
+        if(LSDwhite > 15) { LSDwhite = 0; }
+        if(LSDpink > 15) { LSDpink = 0; }
+        if(LSDpurple > 15) { LSDpurple = 0; }
+    };
+    $scope.enableLSD = function() {
+        $interval($scope.LSD, 25);
+    }
+
 }]);
